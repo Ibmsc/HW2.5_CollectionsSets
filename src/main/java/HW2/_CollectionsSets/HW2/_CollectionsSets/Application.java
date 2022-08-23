d14bafb77eef984cf2333f41b0907f4f5c0ca6b3
ibmsc <Ibm.sc1409@gmail.com>
2022-08-23 18:54:33 +0300

    Initial commit

---
+++ src/main/java/HW2/_CollectionsSets/HW2/_CollectionsSets/Application.java
@@ -0,0 +1,13 @@
+package HW2._CollectionsSets.HW2._CollectionsSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/HW2/_CollectionsSets/HW2/_CollectionsSets/ApplicationTests.java
@@ -0,0 +1,13 @@
+package HW2._CollectionsSets.HW2._CollectionsSets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
